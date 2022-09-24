58f5b598a1e6425ba7cb89570c55ec0621246f82
minhtan0804 <minhtan290902@gmail.com>
2022-09-24 23:27:54 +0700

    Tao notification card va setting screen

---
+++ app/src/main/java/com/example/timviec/components/NotificationCard.java
@@ -0,0 +1,72 @@
+package com.example.timviec.components;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
+import android.text.TextPaint;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import com.example.timviec.R;
+
+/**
+ * TODO: document your custom view class.
+ */
+public class NotificationCard extends FrameLayout {
+    private String mTitle;
+    private String mContent;
+
+
+    public NotificationCard(Context context) {
+        super(context);
+        init(null, 0);
+    }
+
+    public NotificationCard(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs, 0);
+    }
+
+    public NotificationCard(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(attrs, defStyle);
+    }
+
+    private void init(AttributeSet attrs, int defStyle) {
+        inflate(getContext(), R.layout.sample_notification_card, this);
+
+        // Load attributes
+        final TypedArray a = getContext().obtainStyledAttributes(
+                attrs, R.styleable.NotificationCard, defStyle, 0);
+
+        mTitle = a.getString(R.styleable.NotificationCard_notification_card_title);
+        mContent = a.getString(R.styleable.NotificationCard_notification_card_content);
+
+        ((TextView) findViewById(R.id.notification_card_title)).setText(mTitle);
+        ((TextView) findViewById(R.id.notification_card_content)).setText(mContent);
+
+        a.recycle();
+    }
+
+    public String getmTitle() {
+        return mTitle;
+    }
+
+    public void setmTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+
+    public String getmContent() {
+        return mContent;
+    }
+
+    public void setmContent(String mContent) {
+        this.mContent = mContent;
+    }
+}
+
---
+++ app/src/main/java/com/example/timviec/views/SettingScreen.java
@@ -0,0 +1,17 @@
+package com.example.timviec.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.timviec.R;
+import com.example.timviec.Utils;
+
+public class SettingScreen extends Utils.BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting_screen);
+    }
+}
