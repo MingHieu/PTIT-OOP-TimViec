6714b90900082c420f9c70d8758c2a16291f4906
MingHieu <hieubeo0612@gmail.com>
2022-09-27 11:45:07 +0700

    Modify StateManagerService

--- app/src/main/java/com/example/timviec/services/StateManagerService.java
+++ app/src/main/java/com/example/timviec/services/StateManagerService.java
@@ -1,28 +1,53 @@
 package com.example.timviec.services;
 
+import android.content.Context;
+
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
+
 import com.example.timviec.model.User;
 
 public class StateManagerService {
-    private User user;
-    private String authToken;
+    private MutableLiveData<User> user = new MutableLiveData<>();
+    private MutableLiveData<String> authToken = new MutableLiveData<>();
 
     public StateManagerService() {
-        user = new User(10, null, null, null);
+        user.setValue(new User(10, null, null, null));
+        authToken.setValue("");
     }
 
     public User getUser() {
-        return user;
+        return user.getValue();
     }
 
     public void setUser(User user) {
-        this.user = user;
+        this.user.setValue(user);
+    }
+
+    public void setOnChangeUser(Context context, Runnable runnable) {
+        user.observe((LifecycleOwner) context, new Observer<User>() {
+            @Override
+            public void onChanged(User user) {
+                runnable.run();
+            }
+        });
     }
 
     public String getAuthToken() {
-        return authToken;
+        return authToken.getValue();
     }
 
     public void setAuthToken(String authToken) {
-        this.authToken = authToken;
+        this.authToken.setValue(authToken);
+    }
+
+    public void setOnChangeAuthToken(Context context, Runnable runnable) {
+        authToken.observe((LifecycleOwner) context, new Observer<String>() {
+            @Override
+            public void onChanged(String authToken) {
+                runnable.run();
+            }
+        });
     }
 }
