535024f0d96419c8a31c9836d771dd50a6879ed7
MingHieu <hieubeo0612@gmail.com>
2022-09-28 21:54:47 +0700

    Create LoadingDialog, CustomDialog. Fix fontsize CustomInput type password

---
+++ app/src/main/java/com/example/timviec/components/LoadingDialog.java
@@ -0,0 +1,34 @@
+package com.example.timviec.components;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.view.LayoutInflater;
+
+import com.example.timviec.R;
+
+public class LoadingDialog {
+    Activity activity;
+    AlertDialog alertDialog;
+
+    public LoadingDialog(Activity activity ) {
+        this.activity = activity;
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+        LayoutInflater inflater = activity.getLayoutInflater();
+        builder.setView(inflater.inflate(R.layout.loading_dialog, null));
+        builder.setCancelable(false);
+
+        alertDialog = builder.create();
+        alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+    }
+
+    public void show() {
+        alertDialog.show();
+    }
+
+    public void hide() {
+        alertDialog.dismiss();
+    }
+}
