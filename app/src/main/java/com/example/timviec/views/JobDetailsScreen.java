e18038e822727f1472712923ec8a994ec17b4bd0
minhtan0804 <minhtan290902@gmail.com>
2022-10-04 15:12:36 +0700

    Delete LogoutScreen.java

---
+++ app/src/main/java/com/example/timviec/views/JobDetailsScreen.java
@@ -0,0 +1,26 @@
+package com.example.timviec.views;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.timviec.R;
+import com.example.timviec.Utils;
+import com.example.timviec.components.CustomButton;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+public class JobDetailsScreen extends Utils.BaseActivity {
+    private CustomButton m;
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_job_details);
+//    }
+
+
+}
