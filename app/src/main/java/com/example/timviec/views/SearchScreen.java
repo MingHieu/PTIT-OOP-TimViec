aa41f1561c5cb0d32c751b7f6eb04c53655bacbb
minhtan0804 <minhtan290902@gmail.com>
2022-10-08 23:12:05 +0700

    Creat UI search screen activity

---
+++ app/src/main/java/com/example/timviec/views/SearchScreen.java
@@ -0,0 +1,16 @@
+package com.example.timviec.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.timviec.R;
+
+public class SearchScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_screen);
+    }
+}
