3f334f3bc54491f07af5cf185867dab2922c854c
MingHieu <hieubeo0612@gmail.com>
2022-10-17 01:54:39 +0700

    Create Storage Service

---
+++ app/src/main/java/com/example/timviec/services/StorageService.java
@@ -0,0 +1,44 @@
+package com.example.timviec.services;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class StorageService {
+    private SharedPreferences sharedPref;
+
+    public StorageService(Context context) {
+        sharedPref = context.getSharedPreferences("com.example.timviec", Context.MODE_PRIVATE);
+    }
+
+    public void setString(String key, String value) {
+        sharedPref.edit().putString(key, value).apply();
+    }
+
+    public String getString(String key) {
+        return sharedPref.getString(key, null);
+    }
+
+    public void setBoolean(String key, Boolean value) {
+        sharedPref.edit().putBoolean(key, value).apply();
+    }
+
+    public Boolean getBoolean(String key) {
+        return sharedPref.getBoolean(key, false);
+    }
+
+    public void setLong(String key, long value) {
+        sharedPref.edit().putLong(key, value).apply();
+    }
+
+    public long getLong(String key) {
+        return sharedPref.getLong(key, 0);
+    }
+
+    public void setFloat(String key, Float value) {
+        sharedPref.edit().putFloat(key, value).apply();
+    }
+
+    public float getFloat(String key) {
+        return sharedPref.getFloat(key, 0);
+    }
+}
