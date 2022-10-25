5ddfdb237d3617072bcb1bdce5b57ea3506ac6bd
MingHieu <hieubeo0612@gmail.com>
2022-10-25 16:47:46 +0700

    fix crash noti

--- src/main/java/com/findjb/findjob/Fcm/FCMService.java
+++ src/main/java/com/findjb/findjob/Fcm/FCMService.java
@@ -6,13 +6,16 @@ import com.findjb.findjob.Request.FcmNotification;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.Notification;
 
 @Service
 public class FCMService {
     public String pushNotification(FcmNotification pnsRequest) {
         Message message = Message.builder()
-                .setTitle(pnsRequest.getTitle())
-                .setBody(pnsRequest.getBody())
+                .setNotification(Notification.builder()
+                    .setTitle(pnsRequest.getTitle())
+                    .setBody(pnsRequest.getBody())
+                    .build())
                 .setToken(pnsRequest.getFcmToken())
                 .build();
 
